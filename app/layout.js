8efda5e3cd5650ccb473fabdae5d2798632e4c60
Jerry Nelson <jerrycobbinah775@gmail.com>
2025-07-22 15:50:00 +0000

    Final Google Analytics integration using next/script

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import Script from "next/script";
 
 export const metadata = {
   title: "Jerry Nelson | Data Analyst Portfolio",
@@ -9,6 +10,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YDE9MGHYK0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YDE9MGHYK0', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body>
         <Navbar />
         <main>{children}</main>
